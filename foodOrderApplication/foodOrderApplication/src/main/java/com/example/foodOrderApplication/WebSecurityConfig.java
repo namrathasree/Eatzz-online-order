a5827947ca4845f21f8d5736d68277e6c3e0b232
Namrathasree <namrathasree3@gmail.com>
2022-02-03 12:49:34 +0530

    security error

---
+++ foodOrderApplication/foodOrderApplication/src/main/java/com/example/foodOrderApplication/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+//package com.example.foodOrderApplication;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/", "/home","/register").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/admin")
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+//    }
+//
+//
+//}
--- foodOrderApplication/foodOrderApplication/src/main/java/com/example/foodOrderApplication/controller/AdminController.java
+++ foodOrderApplication/foodOrderApplication/src/main/java/com/example/foodOrderApplication/controller/AdminController.java
@@ -1,8 +1,30 @@
 package com.example.foodOrderApplication.controller;
 
+import com.example.foodOrderApplication.entity.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class AdminController {
 
+    @PostMapping("/adminHome")
+    public String adminLogin(HttpServletRequest request, Model model) {
+        String adminUserName = request.getParameter("adminuserName");
+        String adminPassword = request.getParameter("adminpassword");
+        if (adminUserName.equals("admin@123") && adminPassword.equals("admin")) {
+            return "adminHome";
+        }
+        else{
+            model.addAttribute("message","Invalid Credentials");
+            return "admin";
+        }
 
+    }
+
+//    @PostMapping("/")
 }
+
